a62587f29fbbd781ccd57198013d28afca20617b
AntonioFoti02 <antonio.foti@studenti.unicam.it>
2025-04-18 23:30:32 +0200

    Aggiungere i file di progetto.

---
+++ WebApplicationAreeVerdiMilanoProgetto/Controllers/AreaVerdeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApplicationAreeVerdiMilanoProgetto.Models;
+
+namespace WebApplicationAreeVerdiMilanoProgetto.Controllers
+{
+    public class AreeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            string url = "https://dati.comune.milano.it/dataset/da6bc86d-c27f-4256-84eb-86c35dad7d0e/resource/3fe3c952-c632-4b8e-a4a3-616ef39414a0/download/ds339_territorioambiente_aree-verdi-zona-superficie_2014.json";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var json = await client.GetStringAsync(url);
+                var aree = JsonConvert.DeserializeObject<List<AreaVerde>>(json);
+                return View(aree);
+            }
+        }
+
+        // Metodo per la ricerca
+        public async Task<IActionResult> Search(string localita)
+        {
+            string url = "https://dati.comune.milano.it/dataset/da6bc86d-c27f-4256-84eb-86c35dad7d0e/resource/3fe3c952-c632-4b8e-a4a3-616ef39414a0/download/ds339_territorioambiente_aree-verdi-zona-superficie_2014.json";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var json = await client.GetStringAsync(url);
+                var aree = JsonConvert.DeserializeObject<List<AreaVerde>>(json);
+
+                // Se il campo di ricerca è vuoto restituisce tutte le aree
+                if (string.IsNullOrWhiteSpace(localita))
+                {
+                    return View("Index", aree);  // Non eseguiamo alcun filtro
+                }
+
+                // Altrimenti, filtra le aree in base alla località
+                var risultati = aree
+                    .Where(a => a.Nomelocalità != null && a.Nomelocalità.Contains(localita, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+
+                return View("Index", risultati);
+            }
+        }
+    }
+}
---
+++ WebApplicationAreeVerdiMilanoProgetto/Models/AreaVerdejson.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace WebApplicationAreeVerdiMilanoProgetto.Models
+{
+    public class AreaVerde
+    {
+        public string? Zona { get; set; }
+        public string? Tipo { get; set; }
+        public string? Area { get; set; }
+        public string? Classificazione { get; set; }
+        public string? AFFIDATARIO { get; set; }
+
+        [JsonProperty("Superficie totale in mq")]
+        public string? Superficietotaleinmq { get; set; }
+
+        public string? Descrizione { get; set; }
+
+        [JsonProperty("ID Località")]
+        public string? IDLocalità { get; set; }
+
+        [JsonProperty("Nome località")]
+        public string? Nomelocalità { get; set; }
+
+        [JsonProperty("Classificazione ISTAT")]
+        public string? ClassificazioneISTAT { get; set; }
+    }
+}
